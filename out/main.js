28cf5acaad2662f142a0f127e596d96eb8b2880f
HarriKnox <harri.alpha96@gmail.com>
2015-12-10 11:24:10 -0800

    Reverted base.js

--- out/main.js
+++ out/main.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script src="https://closure-library.googlecode.com/git-history/6a51959b7f732ee1f568eaf8dfde0522b8d2d0c2/closure/goog/base.js"></script>');
+if(typeof goog == "undefined") document.write('<script src="https://raw.githubusercontent.com/HarriKnox/IntegerString/master/out/goog/base.js"></script>');
 document.write('<script src="https://raw.githubusercontent.com/HarriKnox/IntegerString/master/out/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog != "undefined") { goog.require("io.knox.IntegerString"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
