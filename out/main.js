5e1518ac701bd992835fa0ea279289a31c8e3949
HarriKnox <harri.alpha96@gmail.com>
2016-01-08 19:34:07 -0800

    Made too many changes to document anything. Starting over here.

--- out/main.js
+++ out/main.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script src="https://raw.githubusercontent.com/HarriKnox/IntegerString/master/out/goog/base.js"></script>');
-document.write('<script src="https://raw.githubusercontent.com/HarriKnox/IntegerString/master/out/cljs_deps.js"></script>');
-document.write('<script>if (typeof goog != "undefined") { goog.require("io.knox.IntegerString"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
+if(typeof goog == "undefined") document.write('<script src="out/goog/base.js"></script>');
+document.write('<script src="out/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.IntegerString"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
