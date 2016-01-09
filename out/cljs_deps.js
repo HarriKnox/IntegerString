66a21435e5d47664593668162edec22afaf412e0
HarriKnox <harri.alpha96@gmail.com>
2016-01-08 20:05:39 -0800

    Built IntegerString for ClojureScript

---
+++ out/cljs_deps.js
@@ -0,0 +1,4 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.object', 'goog.string.StringBuffer', 'goog.array']);
+goog.addDependency("../clojure/string.js", ['clojure.string'], ['goog.string', 'cljs.core', 'goog.string.StringBuffer']);
+goog.addDependency("../io/harriknox/IntegerString.js", ['io.harriknox.IntegerString'], ['cljs.core', 'clojure.string']);
