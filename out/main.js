713c7a85792c6da7e6aa3f92966a832011d6c32a
HarriKnox <harri.alpha96@gmail.com>
2016-03-23 02:46:11 -0700

    Fixed main.js to work with websites

--- out/main.js
+++ out/main.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script src="out/goog/base.js"></script>');
-document.write('<script src="out/cljs_deps.js"></script>');
+if(typeof goog == "undefined") document.write('<script src="https://raw.githubusercontent.com/HarriKnox/Number-Name/master/out/goog/base.js"></script>');
+document.write('<script src="https://raw.githubusercontent.com/HarriKnox/Number-Name/master/out/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberName"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
