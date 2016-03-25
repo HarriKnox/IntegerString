14ddd3b133eab25deb4306d94f2a0bc9b016e7d5
HarriKnox <harri.alpha96@gmail.com>
2016-03-25 14:20:56 -0700

    Used appropriate commit hashes for RawGit

--- out/main.js
+++ out/main.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script type="text/javascript" src="https://cdn.rawgit.com/HarriKnox/Number-Name/master/out/goog/base.js"></script>');
-document.write('<script type="text/javascript" src="https://cdn.rawgit.com/HarriKnox/Number-Name/master/out/cljs_deps.js"></script>');
+if(typeof goog == "undefined") document.write('<script type="text/javascript" src="https://cdn.rawgit.com/HarriKnox/Number-Name/66a21435e5d47664593668162edec22afaf412e0/out/goog/base.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.rawgit.com/HarriKnox/Number-Name/bda3a216f7a7ed95b1e2a991ea865200902496d0/out/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberName"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
