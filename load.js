1d185bbcc321139c667db82b70c82ee0f96d297f
HarriKnox <harri.alpha96@gmail.com>
2016-03-30 17:20:54 -0700

    Added a 'load' script so you can use <url>/load.js the project into your website, but also use out/main.js in a local project

---
+++ load.js
@@ -0,0 +1,4 @@
+var CLOSURE_UNCOMPILED_DEFINES = null;
+if(typeof goog == "undefined") document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/3cd5c264f2443b77b93af2785788bc6229924919/out/goog/base.js"></script>');
+document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/3cd5c264f2443b77b93af2785788bc6229924919/out/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberName"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
