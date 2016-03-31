30822145746329987145b21a49f0b0ec31810517
HarriKnox <harri.alpha96@gmail.com>
2016-03-30 17:37:38 -0700

    Updated links

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/goog/base.js"></script>');
-document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Name/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/cljs_deps.js"></script>');
-document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberName"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
+if(typeof goog == "undefined") document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/goog/base.js"></script>');
+document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberNamer"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
