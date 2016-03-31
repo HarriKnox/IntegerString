c568c10a0f1c8da4d7f1d54d6254cae02de3a249
HarriKnox <harri.alpha96@gmail.com>
2016-03-30 17:39:42 -0700

    Updated links in load.js

--- load.js
+++ load.js
@@ -1,4 +1,4 @@
 var CLOSURE_UNCOMPILED_DEFINES = null;
-if(typeof goog == "undefined") document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/goog/base.js"></script>');
-document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/dbf8d225cde1e3ca62875eb3cba53eb9980df338/out/cljs_deps.js"></script>');
+if(typeof goog == "undefined") document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/9f95208dd81ae60df5b22b5cf53bb73b8bf42d24/out/goog/base.js"></script>');
+document.write('<script src="https://cdn.rawgit.com/HarriKnox/Number-Namer/9f95208dd81ae60df5b22b5cf53bb73b8bf42d24/out/cljs_deps.js"></script>');
 document.write('<script>if (typeof goog != "undefined") { goog.require("io.harriknox.NumberNamer"); } else { console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?"); };</script>');
